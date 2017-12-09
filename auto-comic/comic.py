be96514ed9eba28148a504abb01f9d033cfeb250
fouqau4 <fouqau4@gmail.com>
2017-12-10 01:30:37 +0800

    Automatically check the updating of favor comics and open the page in browser

---
+++ auto-comic/comic.py
@@ -0,0 +1,39 @@
+import json
+import requests
+import time
+import webbrowser
+
+with open( 'comics.json' ) as f:
+	comics = json.load( f )
+	
+modified = False
+chrome_app_path='c/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+while True :
+	print 'check'
+
+	for comic in comics :
+		protocol = 'http://'
+		url = 'www.comicbus.com/html/' + str( comic['comic_id'] ) + '.html'
+		request = protocol + url
+
+		response = requests.get( request )
+
+		pattern = str( comic['comic_id'] ) + '-' + str( comic['newest'] + 1 ) + '.html'
+		pos = response.content.find( pattern )
+
+		if pos == -1 :
+			pass
+#			print comic['name'].encode('utf8') + ' is not updated.'
+		else :
+			comic['newest'] = comic['newest'] + 1
+			print '[updated]' + comic['name'].encode('utf8') + '[' + str( comic['newest'] ) + '] is now available.'
+			modified = True
+			open_url='http://v.comicbus.com/online/comic-'+ str( comic['comic_id'] ) + '.html?ch=' + str( comic['newest'] ) + ''
+			webbrowser.Chrome( chrome_app_path ).open_new_tab( open_url )
+
+	if modified == True :
+		with open( 'comics.json', 'w' ) as f:
+			json.dump( comics, f )
+		modified = False
+	print 'sleep'
+	time.sleep( 10 )
