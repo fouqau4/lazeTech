b9aad21eb603e990bea845afdf70d0097413795e
Adrian Sieber <mail@adriansieber.com>
2018-02-21 22:40:23 +0000

    Fix code style

--- auto-comic/comic.py
+++ auto-comic/comic.py
@@ -3,38 +3,41 @@ import requests
 import time
 import webbrowser
 
-with open( 'comics.json' ) as f:
-	comics = json.load( f )
-	
+with open('comics.json') as f:
+    comics = json.load(f)
+
 modified = False
-chrome_app_path='/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe'
-while True :
-	print 'check'
+chrome_app_path = '/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+while True:
+    print 'check'
 
-	for comic in comics :
-		protocol = 'http://'
-		url = 'www.comicbus.com/html/' + str( comic['comic_id'] ) + '.html'
-		request = protocol + url
+    for comic in comics:
+        protocol = 'http://'
+        url = 'www.comicbus.com/html/' + str(comic['comic_id']) + '.html'
+        request = protocol + url
 
-		response = requests.get( request )
+        response = requests.get(request)
 
-		while( True ) :
-			pattern = str( comic['comic_id'] ) + '-' + str( comic['newest'] + 1 ) + '.html'
-			pos = response.content.find( pattern )
+        while(True):
+            pattern = str(comic['comic_id']) + '-' + \
+                str(comic['newest'] + 1) + '.html'
+            pos = response.content.find(pattern)
 
-			if pos == -1 :
-				break
+            if pos == -1:
+                break
 #			print comic['name'].encode('utf8') + ' is not updated.'
-			else :
-				comic['newest'] = comic['newest'] + 1
-				print '[updated]' + comic['name'].encode('utf8') + '[' + str( comic['newest'] ) + '] is now available.'
-				modified = True
-				open_url='http://v.comicbus.com/online/comic-'+ str( comic['comic_id'] ) + '.html?ch=' + str( comic['newest'] ) + ''
-				webbrowser.Chrome( chrome_app_path ).open_new_tab( open_url )
+            else:
+                comic['newest'] = comic['newest'] + 1
+                print '[updated]' + comic['name'].encode('utf8') + '[' + str(comic['newest']) + '] is now available.'
+                modified = True
+                open_url = 'http://v.comicbus.com/online/comic-' + \
+                    str(comic['comic_id']) + '.html?ch=' + \
+                    str(comic['newest']) + ''
+                webbrowser.Chrome(chrome_app_path).open_new_tab(open_url)
 
-	if modified == True :
-		with open( 'comics.json', 'w' ) as f:
-			f.write( json.dumps( comics, indent = 4 ) )
-		modified = False
-	print 'END'
-	break
+    if modified == True:
+        with open('comics.json', 'w') as f:
+            f.write(json.dumps(comics, indent=4))
+        modified = False
+    print 'END'
+    break
