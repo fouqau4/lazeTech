664fef86fee2e26677558dcab6b578258cb87c92
fouqau4 <fouqau4@gmail.com>
2018-10-05 14:09:33 +0800

    add new app

---
+++ jpLearning/vocabulary.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+import sys
+from random import shuffle
+
+_dir_vocs = './vocabulary.json'
+_num = int(sys.argv[1]) if len(sys.argv) > 1 else 10
+
+with open(_dir_vocs) as f:
+	data = json.load(f)
+
+keys = data.keys()
+shuffle(keys)
+
+print sys.stdin.encoding
+
+
+for idx in xrange(min(_num,len(keys))):
+	fail=True
+	for i in xrange(3) :
+		ans = raw_input(keys[idx].encode('utf-8') + " : ")
+		if ans == data[keys[idx]].encode('utf-8'):
+			fail=False
+			break
+	if fail:
+		print data[keys[idx]]	
+	else:
+		print 'O'
+
